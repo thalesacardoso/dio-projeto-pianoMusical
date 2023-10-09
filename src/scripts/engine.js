b9cd8e4ba38d0eb64aeb9edb46bf01ed7aa9907e
thalesacardoso <thales.dev@hotmail.com>
2023-10-09 17:57:58 -0300

    Som das teclas + correções completo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,39 +4,52 @@ GITHUB: https://github.com/thalesacardoso
 LINKEDIN: https://www.linkedin.com/in/thalesacardoso/
 DATA: 
 */
+
+let mapedKeys = [
+   'a', 'w', 's', 'e', 'd',
+   'f', 't', 'g', 'y', 'h',
+   'u', 'j', 'k', 'o', 'l',
+   'p', ';'
+]
+
 let audio = new Audio("./src/audios/a.wav")
 
-const teclas = document.querySelectorAll('.piano-keys .keys')
-let mapedKeys = []
+// Pega todas as teclas do HTML
+let teclas = document.querySelectorAll('.piano-keys .keys')
 
+
+// Evento de click
+teclas.forEach((keys) => {
+   // Dispara o evento quando o mouse é pressionado
+   keys.addEventListener('click', () => {
+      tocarSom(keys.dataset.key)
+   })
+})
+
+// Função resonsável por tocar o som
 const tocarSom = (key) => {
    audio.src = `./src/audios/${key}.wav`
-   audio.play();
    audio.volume = 0.9
-
-
+   audio.play();
 }
 
-teclas.forEach((keys) => {
-   keys.addEventListener('click', () => {
-      mapedKeys.push(keys.dataset.key)
-      console.log(mapedKeys)
-      tocarSom(keys.textContent)
-
-   })
-
-})
-
-
+// Dispara o evento quando o teclado é pressionado
 document.addEventListener("keydown", (e) => {
    const tecla = document.querySelector(`[data-key="${e.key}"]`)
-   tocarSom(e.key)
 
-   tecla.classList.add('click')
+   // verifica se existe a tecla pessionada dentro do array
+   if (mapedKeys.includes(e.key)) {
+      // Função que toca o som da tecla
+      tocarSom(e.key)
 
-   setTimeout(() => {
-      tecla.classList.remove('click')
-   }, 150)
+      // Adiciona efeito nas teclas quando toca
+      tecla.classList.add('click')
+
+      // Remove efeito das teclas após 150 milisegundos
+      setTimeout(() => {
+         tecla.classList.remove('click')
+      }, 150)
+   }
+
 })
 
-console.log(mapedKeys)
